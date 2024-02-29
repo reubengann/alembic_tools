c70d7da7b4c1160893a71ddbe67040adbdf4a0d9
Reuben Gann <reubengann@gmail.com>
2024-02-29 13:44:14 -0500

    Set the squashed result to the same revision id so that future revisions can reference it correctly

--- src/squash.py
+++ src/squash.py
@@ -1,6 +1,4 @@
 import ast
-import importlib.util
-import os
 from pathlib import Path
 import re
 from typing import NamedTuple
@@ -95,8 +93,8 @@ def squash_commits(rev1_prefix: str, rev2_prefix: str, commit_name: str | None) 
     from_rev = revs_to_squash.from_rev
     to_rev = revs_to_squash.to_rev
     print(f"Squashing {from_rev.revision} and {to_rev.revision}")
-    script = script_folder.generate_revision(alembic.util.rev_id(), commit_name)
-    if script is None:
+    new_script = script_folder.generate_revision(alembic.util.rev_id(), commit_name)
+    if new_script is None:
         print("Unable to make script!")
         return 1
     from_rev_path = Path(from_rev.path)
@@ -109,7 +107,7 @@ def squash_commits(rev1_prefix: str, rev2_prefix: str, commit_name: str | None) 
         return 1
     upgrade_1, downgrade_1 = rev1_methods
     upgrade_2, downgrade_2 = rev2_methods
-    script_path = Path(script.path)
+    script_path = Path(new_script.path)
     new_rev_text = script_path.read_text()
     upgrade_1 = upgrade_1.split("def upgrade() -> None:")[1]
     upgrade_2 = upgrade_2.split("def upgrade() -> None:")[1]
@@ -117,20 +115,31 @@ def squash_commits(rev1_prefix: str, rev2_prefix: str, commit_name: str | None) 
     downgrade_2 = downgrade_2.split("def downgrade() -> None:")[1]
     combined_upgrade = f"<<<<<<< {from_rev.revision}\n {upgrade_1}\n=======\n{upgrade_2}\n>>>>>>> {to_rev.revision}"
     combined_downgrade = f"<<<<<<< {to_rev.revision}\n {downgrade_2}\n=======\n{downgrade_1}\n>>>>>>> {from_rev.revision}"
-    print("def upgrade() -> None:" in new_rev_text)
+    if "def upgrade() -> None:\n    pass" not in new_rev_text:
+        raise Exception("Could not match upgrade string in source file")
     new_rev_text = new_rev_text.replace(
         "def upgrade() -> None:\n    pass",
         f"def upgrade() -> None:\n{combined_upgrade}",
     )
+    if "def downgrade() -> None:\n    pass" not in new_rev_text:
+        raise Exception("Could not match downgrade string in source file")
     new_rev_text = new_rev_text.replace(
         "def downgrade() -> None:\n    pass",
         f"def downgrade() -> None:\n{combined_downgrade}",
     )
-    print('down_revision: Union[str, None] = "[^"\\\']"')
     new_rev_text = re.sub(
         R'down_revision: Union\[str, None\] = ["\'][^"\']+["\']',
         f"down_revision: Union[str, None] = {repr(from_rev.down_revision)}",
         new_rev_text,
+    )
+    new_rev_text = new_rev_text.replace(new_script.revision, to_rev.revision)
+    new_rev_text = re.sub(
+        R"Revises: [^\n]+", f"Revises: {from_rev.down_revision or ''}", new_rev_text
+    )
+    rev_part, msg_part = script_path.name.split("_", maxsplit=1)
+
+    script_path = script_path.rename(
+        script_path.parent / "_".join([to_rev.revision, msg_part])
     )
     script_path.write_text(new_rev_text)
     squashed_folder = Path(".") / "squashed_revisions"
@@ -138,15 +147,17 @@ def squash_commits(rev1_prefix: str, rev2_prefix: str, commit_name: str | None) 
     to_rev_path.rename(squashed_folder / to_rev_path.name)
     from_rev_path.rename(squashed_folder / from_rev_path.name)
     print(
-        f"Commits squashed successfully. You will need to open {script_path.resolve()} and modify it to finish the squash"
-    )
-    print(
-        "If any databases are currently on one of the squashed "
-        "commits, alembic will be unable to run migrations. In that case, you'll have to manually"
-        "update the database using SQL:"
         f"""
-        update alembic_version set version_num = '{script.revision}'
-        """
-        "To undo this change, copy the files from the squashed_revisions folder back into versions and delete the new revision."
+Commits squashed successfully. You will need to open {script_path.resolve()} 
+and modify it to finish the squash. 
+
+If any databases are currently on one of the squashed
+commits, alembic will be unable to run migrations. In that case, you'll have to manually 
+run update the database using SQL:
+
+    update alembic_version set version_num = '{new_script.revision}'
+
+To undo this change, copy the files from the squashed_revisions folder back into versions
+and delete the new revision."""
     )
     return 0
